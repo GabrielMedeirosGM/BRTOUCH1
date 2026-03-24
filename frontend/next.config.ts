679755c3b5640588bfbe54dff6ee8aebc7e2cc05
Gabriel de Medeiros <153210950+GabrielMedeirosGM@users.noreply.github.com>
2026-03-24 01:12:19 -0300

    chore: configurar deploy no github pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/BRTOUCH1',
+  assetPrefix: '/BRTOUCH1',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
